cdecf5362d7fca007c96b577af0184718a3fceb9
Ken Snyder <ken@ken.net>
2016-02-12 23:13:37 +0000

    updated to ember-cli@2.3.0-beta.2

---
+++ ember-cli-build.js
@@ -0,0 +1,13 @@
+/*jshint node:true*/
+/* global require, module */
+var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+
+module.exports = function(defaults) {
+  var app = new EmberAddon(defaults, {
+    // Add options here
+  });
+  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+  app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+
+  return app.toTree();
+};
