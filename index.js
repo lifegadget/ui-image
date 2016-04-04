27966dfd686dc5fa3d5bd96867b4d53e9651d5f9
Ken Snyder <ken@ken.net>
2016-04-04 10:15:26 +0100

    remove unneeded deps

--- index.js
+++ index.js
@@ -7,11 +7,6 @@ module.exports = {
   included: function(app, parentAddon) {
     this._super.included(app);
     const target = (parentAddon || app);
-    const bower = target.bowerDirectory;
     target.import('vendor/ui-image/ui-image.css');
-    target.import(bower + '/fetch/fetch.js');
-    target.import(bower + '/es6-promise/promise.js');
-
-
   }
 };
